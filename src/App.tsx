ee5dc236a2cdcfcf112b6e2db91bc7f762c13f68
Nischal <sainischalmv@gmail.com>
2024-05-01 12:18:42 +0530

    workoutlog

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home";
 import Workoutlog from "./workoutlog";
+import Workoutinfo from "./workoutplans";
 
 function App() {
   return (
@@ -8,13 +9,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {
-            /* <Route path="/login" element={<Login />} />
+          <Route path="/workoutinfo" element={<Workoutinfo />} />
+          <Route path="/workoutlog" element={<Workoutlog />} />
+          {/* <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/foodlog" element={<FoodLog />} />
-          <Route path="/WorkoutPlans" element={<WorkoutPlans />} />*/
-            <Route path="/workoutlog" element={<Workoutlog />} />
-          }
+          <Route path="/foodlog" element={<FoodLog />} />*/}
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/workoutplans.tsx
@@ -0,0 +1,16 @@
+import Paragraphguy from "./assets/custom-componets/paragraphguy";
+import Workoutnames from "./assets/custom-componets/workoutnames";
+import Workoutnamesinfo from "./assets/custom-componets/workoutnamesinfo";
+
+const Workoutinfo = () => {
+  return (
+    <>
+      <Paragraphguy />
+      <Workoutnames />
+      <Workoutnamesinfo />
+      <Workoutnames />
+    </>
+  );
+};
+
+export default Workoutinfo;
