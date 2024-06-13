02b52d0559f9d873bf5ba8e2c6c0a9363c9716c7
Nischal_UwU <118107697+Nischa1Mv@users.noreply.github.com>
2024-06-13 15:41:13 +0530

    tetsing 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Workoutinfo />} />
-          <Route path="/workoutinfo" element={<Home />} />
-          <Route path="/workoutlog" element={<Workoutlog />} />
+          <Route path="/" element={<Home />} />
+          <Route path="workoutinfo" element={<Workoutinfo />} />
+          <Route path="workoutlog" element={<Workoutlog />} />
           {/* <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/foodlog" element={<FoodLog />} />*/}
