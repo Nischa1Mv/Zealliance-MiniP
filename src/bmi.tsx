c8c8942ffc8ddd08e993aef192d945fe6ce75b2c
Nischal_UwU <118107697+Nischa1Mv@users.noreply.github.com>
2024-06-20 10:50:40 +0530

    cllg

--- src/bmi.tsx
+++ src/bmi.tsx
@@ -3,17 +3,8 @@ import InputWithLabel from "./assets/custom-componets/inputwithlabel";
 const Bmi = () => {
   return (
     <>
-      <div className=" rounded-xl py-5 px-8 grid grid-cols-2 gap-10 grid-rows-1 bg-[#f4a261]  ">
-        <div className=" bg-white border-4  border-black rounded-xl px-6 py-10 font-semibold text-xl">
-          {" "}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+      <div className=" rounded-xl py-5 px-8 grid grid-cols-2 gap-10 grid-rows-1 text-black  ">
+        
         <div className=" flex flex-col gap-5 items-center bg-white px-6 py-10  border-4  border-black rounded-xl">
           <div className="text-6xl font-extrabold  ">Enter Your Stats</div>{" "}
           <form action="" className="">
@@ -69,6 +60,16 @@ const Bmi = () => {
               </div>
             </div>
           </form>
+        </div>
+        <div className=" bg-white border-4  border-black rounded-xl px-6 py-10 font-semibold text-xl">
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </div>
     </>
