d9398fe85a88ae252d93ced6e818cf275e31fc46
Nischal <sainischalmv@gmail.com>
2024-06-22 12:13:14 +0530

    workoutlog links 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Footer from "./footer";
 import Home from "./home";
-import { Link } from "react-router-dom";
+import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   const currentDate = new Date();
 
@@ -12,46 +12,52 @@ function App() {
 
   return (
     <>
-      <div id="body" className=" flex flex-col gap-5  bg-[#212121] text-white">
-        <div className="h-screen  ">
-          <div
-            id="nav"
-            className="  bg-[#212121] w-full flex items-center gap-6  h-[12vh] border-b-2 border-x-0 border-t-0 z-50 px-10 pr-6 border-[#464646] sticky top-0 "
-          >
-            <div className="text-xl  font-semibold  order-1 my-6 ">
-              {longDateString}{" "}
-            </div>
-            <div className="grow order-1"></div>
-            <div className="text-5xl font-bold order-2  my-3">
-              <a href="">Zeallience</a>
-            </div>
-            <div className="grow order-3"></div>
-            <div className="text-xl font-semibold order-3 my-6 px-4">
-              <a href="">Workout Plans</a>
-            </div>
-            <div className="text-xl font-semibold order-4 my-6 px-4  ">
-              {" "}
-              {/* <Link to="/workoutlog">Workout Log</Link> */}
-            </div>
-            <div className="text-xl font-semibold order-5 my-6 px-4">
-              <a href="">FoodLog</a>{" "}
-            </div>
+      {" "}
+      <BrowserRouter>
+        <div
+          id="body"
+          className=" flex flex-col gap-5  bg-[#212121] text-white"
+        >
+          <div className="h-screen  ">
+            <div
+              id="nav"
+              className="  bg-[#212121] w-full flex items-center gap-6  h-[12vh] border-b-2 border-x-0 border-t-0 z-50 px-10 pr-6 border-[#464646] sticky top-0 "
+            >
+              <div className="text-xl  font-semibold  order-1 my-6 ">
+                {longDateString}{" "}
+              </div>
+              <div className="grow order-1"></div>
+              <div className="text-5xl font-bold order-2  my-3">
+                <a href="">Zeallience</a>
+              </div>
+              <div className="grow order-3"></div>
+              <div className="text-xl font-semibold order-3 my-6 px-4">
+                <Link to="/workoutinfo">Workout Info</Link>{" "}
+              </div>
+              <div className="text-xl font-semibold order-4 my-6 px-4  ">
+                {" "}
+                <a href="">Workout Log</a>
+              </div>
+              <div className="text-xl font-semibold order-5 my-6 px-4">
+                <a href="">FoodLog</a>{" "}
+              </div>
 
-            <div className="order-6  mx-auto flex justify-center items-center rounded-full border-4 border-[#464646]">
-              <img
-                src="https://i.pinimg.com/564x/e3/92/1a/e3921a9e19d508c55dd3c217b9d68db2.jpg"
-                alt=""
-                width="70"
-                className="rounded-full  border-[#464646]"
-              />
+              <div className="order-6  mx-auto flex justify-center items-center rounded-full border-4 border-[#464646]">
+                <img
+                  src="https://i.pinimg.com/564x/e3/92/1a/e3921a9e19d508c55dd3c217b9d68db2.jpg"
+                  alt=""
+                  width="70"
+                  className="rounded-full  border-[#464646]"
+                />
+              </div>
+            </div>
+            <div className=" mx-10  h-full">
+              <Home />
             </div>
           </div>
-          <div className=" mx-10  h-full">
-            <Home />
-          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </BrowserRouter>
     </>
   );
 }
--- src/home.tsx
+++ src/home.tsx
@@ -1,22 +1,20 @@
-import { BrowserRouter, Route, Routes, } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Workoutlog from "./workoutlog";
 import Workoutinfo from "./workoutplans";
-
 import Bmi from "./bmi";
 import Foodlog from "./foodlog";
+
 function Home() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Bmi />} />
-          <Route path="/workoutinfo" element={<Workoutinfo />} />
-          <Route path="workoutlog" element={<Workoutlog />} />
-          <Route path="/foodlog" element={<Foodlog />} />
-          {/* <Route path="/login" element={<Login />} />
+      <Routes>
+        <Route path="/" element={<Bmi />} />
+        <Route path="/workoutinfo" element={<Workoutinfo />} />
+        {/* <Route path="workoutlog" element={<Workoutlog />} />
+          <Route path="/foodlog" element={<Foodlog />} /> */}
+        {/* <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />*/}
-        </Routes>
-      </BrowserRouter>
+      </Routes>
     </>
   );
 }
