eb2a345616d3990f58408ec267a35b1e50e29016
Nischal <sainischalmv@gmail.com>
2024-06-25 11:09:00 +0530

    vercel fix

--- src/context/sharedState.tsx
+++ src/context/sharedState.tsx
@@ -1,5 +1,5 @@
 // SharedStateContext.tsx
-import React, { createContext, useState, useContext, ReactNode } from "react";
+import { createContext, useState, useContext, ReactNode } from "react";
 
 interface SharedStateContextProps {
   isInfo: boolean;
