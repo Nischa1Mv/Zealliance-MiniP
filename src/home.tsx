4ac3be362a5593b6e250f1594cb34499842ca1da
Nischal <sainischalmv@gmail.com>
2024-07-09 11:16:28 +0530

    workout log link added

--- src/home.tsx
+++ src/home.tsx
@@ -2,17 +2,18 @@ import { Route, Routes } from "react-router-dom";
 
 import Workoutinfo from "./workoutplans";
 import Bmi from "./bmi";
+import Workoutlog from "./workoutlog";
 
 function Home() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Bmi />} />
-        <Route path="/workoutinfo" element={<Workoutinfo />} />
-        {/* <Route path="workoutlog" element={<Workoutlog />} />
-          <Route path="/foodlog" element={<Foodlog />} /> */}
-        {/* <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />*/}
+        <Route path="workoutinfo" element={<Workoutinfo />} />
+        <Route path="workoutlog" element={<Workoutlog />} />
+        {/* <Route path="/foodlog" element={<Foodlog />} /> 
+         <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} /> */}
       </Routes>
     </>
   );
