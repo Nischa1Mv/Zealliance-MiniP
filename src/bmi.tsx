45ebd22f81c2bbccd93afaf56b6c2bc587fb5e77
Nischal <sainischalmv@gmail.com>
2024-07-13 12:12:30 +0530

    bmi backend

--- src/bmi.tsx
+++ src/bmi.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 
 interface Props {
   placeholder: string;
   label: string;
   type: string;
   unit: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const InputWithLabel: React.FC<Props> = ({
@@ -12,6 +14,8 @@ const InputWithLabel: React.FC<Props> = ({
   label,
   type,
   unit,
+  value,
+  onChange,
 }) => {
   return (
     <div className="flex w-full max-w-sm  gap-5  ">
@@ -26,6 +30,9 @@ const InputWithLabel: React.FC<Props> = ({
           className="  py-1 px-4 rounded-lg w-full placeholder:text-lg text-lg placeholder:font-semibold focus:outline-none  bg-background     [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
           type={type}
           placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          required
         />
         <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-xl font-semibold text-gray-500">
           {" "}
@@ -50,17 +57,45 @@ const Bmi = () => {
     setIsFemale(true);
   };
 
+  const [height, setHeight] = useState<string>("");
+  const [weight, setWeight] = useState<string>("");
+  const [age, setAge] = useState<string>("");
+  const [bmi, setBmi] = useState<string | null>(null);
+  const [bmiCategory, setBmiCategory] = useState<string | null>(null);
+
+  const calBmi = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (height && weight) {
+      const heightInMeters = parseFloat(height) / 100;
+      const calBmi = parseFloat(weight) / (heightInMeters * heightInMeters);
+      setBmi(calBmi.toFixed(1));
+    } else {
+      setBmi(null);
+    }
+
+    const intbmi = parseInt(bmi, 10);
+    if (intbmi < 18.5) {
+      setBmiCategory("underWeight");
+    } else if (intbmi >= 18.5 && intbmi < 24.9) {
+      setBmiCategory("Normal Weight");
+    } else if (intbmi >= 25 && intbmi <= 29.9) {
+      setBmiCategory("OverWeight Weight");
+    } else {
+      setBmiCategory("Obesity");
+    }
+  };
+
   return (
     <>
       <div className="rounded-xl py-5 px-8 flex  text-black mt-8 gap-4">
         <div className="flex flex-col gap-5 items-center bg-white px-6 py-10 border-4 rounded-xl w-[50%]   ">
           <div className="text-6xl font-extrabold">Enter Your Stats</div>
-          <form action="Submit">
+          <form action="Submit" onSubmit={calBmi}>
             <div className="flex flex-col gap-12 text-3xl font-bold mt-8">
               <div className="grid grid-cols-2 grid-rows-1 text-center">
                 <div
                   className={`border-2 border-black rounded-lg py-2 cursor-pointer  transition duration-200  ${
-                    isMale ? "bg-blue-400 text-white" : "bg-transparent"
+                    isMale ? "bg-blue-500 text-white" : "bg-transparent"
                   }`}
                   onClick={handleMaleClick}
                 >
@@ -68,7 +103,7 @@ const Bmi = () => {
                 </div>
                 <div
                   className={`border-2 border-black rounded-lg py-2 cursor-pointer  transition duration-200 ${
-                    isFemale ? "bg-red-200 text-white" : "bg-transparent"
+                    isFemale ? "bg-red-400 text-white" : "bg-transparent"
                   }`}
                   onClick={handleFemaleClick}
                 >
@@ -82,6 +117,8 @@ const Bmi = () => {
                   placeholder="Age"
                   label="Age"
                   unit="in yrs"
+                  value={age}
+                  onChange={(event) => setAge(event.target.value)}
                 />
               </div>
               <div>
@@ -89,7 +126,9 @@ const Bmi = () => {
                   type="number"
                   placeholder="Height"
                   label="Height"
-                  unit="in ft"
+                  unit="in cm"
+                  value={height}
+                  onChange={(event) => setHeight(event.target.value)}
                 />
               </div>
               <div>
@@ -98,26 +137,24 @@ const Bmi = () => {
                   placeholder="Weight"
                   label="Weight"
                   unit="in kgs"
+                  value={weight}
+                  onChange={(event) => setWeight(event.target.value)}
                 />
               </div>
               <div>
-                <input
+                <button
                   className="border-4 border-black rounded-full py-3 w-full hover:bg-[#c27a50] hover:text-white"
                   type="submit"
-                  placeholder="Submit"
-                />
+                >
+                  Submit
+                </button>
               </div>
             </div>
           </form>
         </div>
-        <div className="bg-white  rounded-xl px-6 py-10 font-semibold text-xl w-[50%]  ">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <div className="bg-white  rounded-xl px-6 py-10 font-semibold text-xl w-[50%]   ">
+          BMI = {bmi}
+          Nigga you are {bmiCategory}
         </div>
       </div>
     </>
