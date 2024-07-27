5fa7743df337c2b912a94326c110ed5e871c4151
Nischal <sainischalmv@gmail.com>
2024-07-27 22:18:16 +0530

    workout log code reduction -datacount

--- src/datacount.tsx
+++ src/datacount.tsx
@@ -37,27 +37,32 @@ function Excercisedata({}) {
   return (
     <>
       {/*---------------------------------------------------Excercise Data-------------------------------------------------------------------->*/}
-      {info ? (
-        <div className=" relative flex border-2 border-[#464646] w-full my-6 py-5 px-4 rounded-xl text-xl font-bold  ">
-          <div>Name : ww / rr , ww / rr , ww / rr , PR: ww / rr</div>
-          <div className=" absolute right-5">
-            <svg
-              onClick={handleClick}
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#e8eaed"
-            >
-              <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
-            </svg>
-          </div>
-        </div>
-      ) : (
-        <>
-          {/* ---------------------------------------------------Excercise Data Info--------------------------------------------------------------------> */}{" "}
-          <div className=" flex flex-col gap-6 border-amber-300 w-full mt-6 py-5 px-4 rounded-xl text-xl font-bold border relative ">
-            <div className="flex gap-20 ">
+      <div
+        className={`relative flex border-2 w-full my-6 py-5 px-4 rounded-xl text-xl font-bold gap-2 ${
+          info ? "  border-[#464646]" : "border-amber-300"
+        }`}
+      >
+        {info ? (
+          <>
+            {" "}
+            <div>Name : ww / rr , ww / rr , ww / rr , PR: ww / rr</div>
+            <div className="absolute right-5">
+              <svg
+                onClick={handleClick}
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#e8eaed"
+              >
+                <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
+              </svg>
+            </div>
+          </>
+        ) : (
+          <>
+            {" "}
+            <div className="flex gap-20  ">
               <div className="flex jusitfy-center items-center">
                 Exercise Name{" "}
               </div>{" "}
@@ -95,7 +100,7 @@ function Excercisedata({}) {
                 </div>
               </div>
             </div>
-            <div className="flex gap-6">
+            <div className="flex gap-6 ">
               <div className="flex ">
                 <h1 className="mt-3">Set-1</h1>
                 <DataWandR />
@@ -109,9 +114,9 @@ function Excercisedata({}) {
                 <DataWandR />
               </div>
             </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </>
   );
 }
