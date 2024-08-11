2db2d4a91601d21dfeebd1f9d211d3d8ad4ce7fa
Nischal <sainischalmv@gmail.com>
2024-08-11 15:10:04 +0530

    Header fixed

--- src/head.tsx
+++ src/head.tsx
@@ -6,47 +6,58 @@ const Header = () => {
   const day = String(currentDate.getDate()).padStart(2, "0");
   const month = currentDate.toLocaleString("default", { month: "long" });
   const year = currentDate.getFullYear();
+  const isUser = false;
 
   const longDateString = `${day} ${month} ${year}`;
   return (
     <>
       <nav
         id="nav"
-        className="  bg-[#16171b]  w-full flex items-center gap-6 py-2  border-b-2 border-x-0 border-t-0 z-50 px-10 pr-6 border-[#464646] sticky top-0 "
+        className="text-xl  font-semibold   bg-[#16171b]  w-full flex items-center  z-50 px-6 border-[#464646] sticky top-0 py-6"
       >
-        <div className="text-xl  font-semibold  order-1 my-6  ">
-          {longDateString}{" "}
-        </div>
+        <div className="order-1   ">{longDateString} </div>
         <div className="grow order-1"></div>
-        <div className="text-4xl font-bold order-2  my-3">
+        <div className="text-4xl font-bold order-2 ">
           <Link to="/">Zeallience</Link>
         </div>
         <div className="grow order-3"></div>
-        <div className="text-xl font-semibold order-3 my-6 px-4 ">
+        <div className=" order-3 px-4 mx-2">
           <Link className="underline-animation" to="/workoutinfo">
             Workout Plans
           </Link>{" "}
         </div>
-        <div className="text-xl font-semibold order-4 my-6 px-4  ">
+        <div className=" order-4  px-4 mx-2 ">
           {" "}
           <Link className="underline-animation" to="/workoutlog">
             Workout Log
           </Link>{" "}
         </div>
-        <div className="text-xl font-semibold order-5 my-6 px-4">
+        <div className=" order-5 px-4 mx-2">
           <Link className="underline-animation" to="/foodlog">
             Food Log
           </Link>
         </div>
-
-        <div className="order-6 mx-auto flex justify-center items-center rounded-full border-4 border-[#464646]">
-          <img
-            src="https://i.pinimg.com/564x/e3/92/1a/e3921a9e19d508c55dd3c217b9d68db2.jpg"
-            alt=""
-            width="70"
-            className="rounded-full  border-[#464646]"
-          />
-        </div>
+        {isUser ? (
+          <>
+            <div className="order-6 mx-auto flex justify-center items-center rounded-full border-4 border-[#464646]">
+              <img
+                src="https://i.pinimg.com/564x/e3/92/1a/e3921a9e19d508c55dd3c217b9d68db2.jpg"
+                alt=""
+                width="70"
+                className="rounded-full  border-[#464646]"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              className="order-6  rounded-xl border-[#464646]   border-2 px-2 py-1   hover:bg-gray-600
+            "
+            >
+              <button onClick={() => {}}>Log In</button>
+            </div>
+          </>
+        )}
       </nav>
     </>
   );
