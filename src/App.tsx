fdc8079be0cd76ecd464f906106cca59945525ec
Nischal <sainischalmv@gmail.com>
2024-08-12 23:56:07 +0530

    back to normal

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen flex-col relative">
-        <div className="sticky top-0 z-50">
-          <Header />
-        </div>
-        <div className="min-h-[86vh]">
-          <Home />
+    <>
+      <BrowserRouter>
+        <div className="min-h-screen flex-col  relative">
+          <div className="sticky top-0 z-50  ">
+            <Header />
+          </div>
+          <div className="  min-h-[86vh]  ">
+            <Home />
+          </div>
         </div>
         <Footer />
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/home.tsx
+++ src/home.tsx
@@ -1,21 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
 import Workoutinfo from "./workoutplans";
 import Bmi from "./bmi";
 import Workoutlog from "./workoutlog";
 import Foodlog from "./foodlog";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import Login from "./Login";
 import Signup from "./signup";
 
 function Home() {
   return (
-    <Routes>
-      <Route path="/" element={<Bmi />} />
-      <Route path="workoutinfo" element={<Workoutinfo />} />
-      <Route path="workoutlog" element={<Workoutlog />} />
-      <Route path="foodlog" element={<Foodlog />} />
-      <Route path="login" element={<Login />} />
-      <Route path="signup" element={<Signup />} />
-    </Routes>
+    <>
+      <DndProvider backend={HTML5Backend}>
+        <Routes>
+          <Route path="/" element={<Bmi />} />
+          <Route path="workoutinfo" element={<Workoutinfo />} />
+          <Route path="workoutlog" element={<Workoutlog />} />
+          <Route path="/foodlog" element={<Foodlog />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </DndProvider>
+    </>
   );
 }
 
